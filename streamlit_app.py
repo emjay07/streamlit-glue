b2365c08ce23acf4c57db57ea0160aabc3a73963
Emily Johnson <ejjohnso@amazon.com>
2023-04-14 14:29:04 -0700

    adding s3 reading from output

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import boto3 as boto
 from botocore.exceptions import ClientError
+import s3fs
 
 def create_glue_client(region_id):
     # Create quicksight client
@@ -38,6 +39,17 @@ def get_table(glue_client, catalog_id, database_name, table_name):
         st.write(e)
         return {}
 
+def get_content_from_s3(s3_path):
+    fs = s3fs.S3FileSystem(anon=False)
+
+    @st.cache_data(ttl=600)
+    def read_file(filename):
+        with fs.open(filename) as f:
+            return f.read().decode("utf-8")
+    
+    content = read_file(s3_path)
+    
+    return content
 
 
 # constants
@@ -45,9 +57,16 @@ k_REGION = "us-west-2"
 k_ACCOUNT_ID = "382152459716"
 k_EXAMPLE_DB_NAME = "output-s3-database"
 k_EXAMPLE_TABLE_NAME = "output-table-demo"
+k_EXAMPLE_S3_PATH = "s3://test-catalog-bucket-ejjohnso/run-1681502746485-part-r-00000"
 
 # Streamlit App
 st.title("Stand-In App")
+
+content = get_content_from_s3(k_EXAMPLE_S3_PATH)
+
+for line in content.strip().split("\n"):
+    event, category, date, venue = line.split(",")
+    st.write(f"There is a {category} event {event} on {date} at {venue}")
 
 glue_client = create_glue_client(k_REGION)
 example_db = get_database(glue_client, k_ACCOUNT_ID, k_EXAMPLE_DB_NAME)
